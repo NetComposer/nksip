e756e17591faa1231340fd675c33a885a2009d21
Carlos González Florido <carlosj.gf@gmail.com>
2014-03-06 17:29:36 +0100

    GRUU tests

--- test/gruu_test.erl
+++ test/gruu_test.erl
@@ -44,31 +44,108 @@ start() ->
     ok = sipapp_server:start({basic, server1}, [
         {from, "sip:server1@nksip"},
         registrar,
+        {local_host, "localhost"},
         {transport, {udp, {0,0,0,0}, 5060}},
         {transport, {tls, {0,0,0,0}, 5061}}
     ]),
 
-    ok = sipapp_endpoint:start({basic, client1}, [
+    ok = sipapp_endpoint:start({basic, ua1}, [
         {from, "sip:client1@nksip"},
+        {local_host, "127.0.0.1"},
         {transport, {udp, {0,0,0,0}, 5070}},
         {transport, {tls, {0,0,0,0}, 5071}}
     ]),
 
-    ok = sipapp_endpoint:start({basic, client2}, [
-        {from, "sip:client2@nksip"}]),
+    ok = sipapp_endpoint:start({basic, ua2}, [
+        {from, "sip:client1@nksip"},
+        {local_host, "127.0.0.1"},
+        {transport, {udp, {0,0,0,0}, 5080}},
+        {transport, {tls, {0,0,0,0}, 5081}}
+    ]),
 
-    % nksip_config:put(registrar_min_time, 60),
-
+    nksip_registrar:internal_clear(),
     tests_util:log(debug),
     ?debugFmt("Starting ~p", [?MODULE]).
 
 stop() ->
     ok = sipapp_server:stop({basic, server1}),
-    ok = sipapp_endpoint:stop({basic, client1}),
-    ok = sipapp_endpoint:stop({basic, client2}).
+    ok = sipapp_endpoint:stop({basic, ua1}),
+    ok = sipapp_endpoint:stop({basic, ua2}).
 
 
 register() ->
-    C1 = {basic, client1},
-    nksip_uac:register(C1, "sip:127.0.0.1;transport=tcp", [make_contact]).
+    C1 = {basic, ua1},
+    C2 = {basic, ua2},
+    S1 = {basic, server1},
+    
+    nksip_registrar:internal_clear(),
+
+
+    {ok, 200, [{_, [PC1]}]} =
+        nksip_uac:register(C1, "sip:127.0.0.1", 
+                               [make_contact, {fields, [parsed_contacts]}]),
+    #uri{
+        user = <<"client1">>, 
+        domain = <<"127.0.0.1">>,
+        port = 5070,
+        ext_opts = EOpts1
+    } = PC1,
+    Inst1 = list_to_binary(
+                nksip_lib:unquote(nksip_lib:get_value(<<"+sip.instance">>, EOpts1))),
+    [Pub1] = nksip_parse:ruris(nksip_lib:unquote(
+                            nksip_lib:get_value(<<"pub-gruu">>, EOpts1))),
+    [Tmp1] = nksip_parse:ruris(nksip_lib:unquote(
+                            nksip_lib:get_value(<<"temp-gruu">>, EOpts1))),
+    {ok, Inst1} = nksip_sipapp_srv:get_uuid(C1),
+    #uri{user = <<"client1">>, domain = <<"nksip">>, port = 0} = Pub1,
+    #uri{domain = <<"localhost">>, port=5060} = Tmp1,
+
+
+    {ok, 200, [{_, [PC2, PC1]}]} =
+        nksip_uac:register(C2, "sip:127.0.0.1", 
+                               [make_contact, {fields, [parsed_contacts]}]),
+    #uri{
+        user = <<"client1">>, 
+        domain = <<"127.0.0.1">>,
+        port = 5080,
+        ext_opts = EOpts2
+    } = PC2,
+    Inst2 = list_to_binary(
+                nksip_lib:unquote(nksip_lib:get_value(<<"+sip.instance">>, EOpts2))),
+    [Pub2] = nksip_parse:ruris(nksip_lib:unquote(
+                            nksip_lib:get_value(<<"pub-gruu">>, EOpts2))),
+    [Tmp2] = nksip_parse:ruris(nksip_lib:unquote(
+                            nksip_lib:get_value(<<"temp-gruu">>, EOpts2))),
+    {ok, Inst2} = nksip_sipapp_srv:get_uuid(C2),
+    #uri{user = <<"client1">>, domain = <<"nksip">>, port = 0} = Pub2,
+    #uri{domain = <<"localhost">>, port=5060} = Tmp2,
+
+
+    % Now we have two contacts stored for this AOR
+    [PC2a, PC1a] = nksip_registrar:find(S1, sip, <<"client1">>, <<"nksip">>),
+    PC2 = PC2a#uri{headers=[]},
+    PC1 = PC1a#uri{headers=[]},
+
+    % But we use the Public or Private GRUUs, only one of each
+    [PC1a] = nksip_registrar:find(S1, Pub1),
+    [PC2a] = nksip_registrar:find(S1, Pub2),
+    [PC1a] = nksip_registrar:find(S1, Tmp1),
+    [PC2a] = nksip_registrar:find(S1, Tmp2),
+
+
+
+
+    
+
+
+    ok.
+
+
+
+
+
+
+
+
+
 
