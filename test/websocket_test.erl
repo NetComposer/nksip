89c048646915f6f020a4605f123a3df74c970c9a
Carlos González Florido <carlosj.gf@gmail.com>
2014-03-11 20:06:19 +0100

    Updated test

--- test/websocket_test.erl
+++ test/websocket_test.erl
@@ -28,82 +28,32 @@
 -compile([export_all]).
 
 
-% basic_test_() ->
-%     {setup, spawn, 
-%         fun() -> start() end,
-%         fun(_) -> stop() end,
-%         [
-%             {timeout, 60, fun running/0}, 
-%             {timeout, 60, fun transport/0}, 
-%             {timeout, 60, fun cast_info/0}, 
-%             {timeout, 60, fun stun/0}
-%         ]
-%     }.
-
-
-start() ->
-    tests_util:start_nksip(),
-
-    ok = sipapp_server:start({basic, server1}, [
-        {from, "\"NkSIP Basic SUITE Test Server\" <sip:server1@nksip>"},
-        registrar,
-        {listeners, 10},
-        {supported, []},
-        {transport, {udp, {0,0,0,0}, 5060}},
-        {transport, {tls, {0,0,0,0}, 5061}},
-        {transport, {ws, {0,0,0,0}, 8080}}
-    ]),
-
-    ok = sipapp_endpoint:start({basic, client1}, [
-        {from, "\"NkSIP Basic SUITE Test Client\" <sip:client1@nksip>"},
-        {supported, []},
-        {transport, {udp, {0,0,0,0}, 5070}},
-        {transport, {tls, {0,0,0,0}, 5071}}
-    ]),
-
-    ok = sipapp_endpoint:start({basic, client2}, [
-        {supported, []},
-        {from, "\"NkSIP Basic SUITE Test Client\" <sip:client2@nksip>"},
-        {transport, {ws, {0,0,0,0}, 0}}
-    ]),
-
-    tests_util:log(),
-    ?debugFmt("Starting ~p", [?MODULE]).
-
-
-stop() ->
-    ok = nksip:stop_all(),
-    error = sipapp_server:stop({basic, server1}),
-    error = sipapp_endpoint:stop({basic, client1}),
-    error = sipapp_endpoint:stop({basic, client2}),
-    ok.
-
-
-server() ->
-    Disp1 = [{'_', [{"/", nksip_transport_ws, []}]}],
-    Disp2 = [{'_', [{"/ws", nksip_transport_ws, []}]}],
-    
+start_test() ->
     ok = nksip:start(ws_a, nksip_sipapp, [], [
         {transport, {ws, {0,0,0,0}, 8090, []}},
         {transport, {wss, {0,0,0,0}, 8091, []}},
-        {transport, {ws, {0,0,0,0}, 0, [{dispatch, Disp2}]}}
+        {transport, {ws, {0,0,0,0}, 0, [{dispatch, "/ws"}]}}
     ]),
     ok = nksip:start(ws_b, nksip_sipapp, [], [
         {transport, {ws, {0,0,0,0}, 8090, []}},
-        {transport, {wss, {0,0,0,0}, 8092, [{dispatch, Disp2}]}}
+        {transport, {wss, {0,0,0,0}, 8092, [{dispatch, [{'_', ["/ws"]}]}]}}
     ]),
-
-
+   
     [
-        {#transport{proto=ws, local_port=0, listen_port=_LP, dispatch=Disp2}, _},
-        {#transport{proto=ws, local_port=8090, listen_port=8090, dispatch=Disp1}, _},
-        {#transport{proto=wss, local_port=8091, listen_port=8091, dispatch=Disp1}, _}
+        {#transport{proto=ws, local_port=0, listen_port=_LP, 
+                    dispatch=[{'_', [], [{"/ws", [], _, _}]}]}, _},
+        {#transport{proto=ws, local_port=8090, listen_port=8090, 
+                    dispatch=[{'_', [], [{"/", [], _, _}]}]}, _},
+        {#transport{proto=wss, local_port=8091, listen_port=8091, 
+                    dispatch=[{'_', [], [{"/", [], _, _}]}]}, _}
     ] = 
         lists:sort(nksip_transport:get_all(ws_a)),
 
     [
-        {#transport{proto=ws, local_port=8090, listen_port=8090, dispatch=Disp1}, _},
-        {#transport{proto=wss, local_port=8092, listen_port=8092, dispatch=Disp2}, _}
+        {#transport{proto=ws, local_port=8090, listen_port=8090, 
+                    dispatch=[{'_', [], [{"/", [], _, _}]}]}, _},
+        {#transport{proto=wss, local_port=8092, listen_port=8092, 
+                    dispatch=[{'_', [], [{"/ws", [], _, _}]}]}, _}
     ] = 
         lists:sort(nksip_transport:get_all(ws_b)),
 
@@ -126,6 +76,63 @@ server() ->
     [] = nksip_transport:get_all(ws_b),
     [] = lists:sort(nksip_webserver_sup:get_all()),
     ok.
+
+
+% basic_test_() ->
+%     {setup, spawn, 
+%         fun() -> start() end,
+%         fun(_) -> stop() end,
+%         [
+%             {timeout, 60, fun running/0}, 
+%             {timeout, 60, fun transport/0}, 
+%             {timeout, 60, fun cast_info/0}, 
+%             {timeout, 60, fun stun/0}
+%         ]
+%     }.
+
+
+start() ->
+    tests_util:start_nksip(),
+
+    ok = sipapp_server:start({ws, server1}, [
+        {from, "\"NkSIP Basic SUITE Test Server\" <sip:server1@nksip>"},
+        registrar,
+        {listeners, 10},
+        {transport, {udp, {0,0,0,0}, 5060}},
+        {transport, {tls, {0,0,0,0}, 5061}},
+        {transport, {ws, {0,0,0,0}, 8080}},
+        {transport, {wss, {0,0,0,0}, 8081, [{dispatch, "/wss"}]}}
+    ]),
+
+    ok = sipapp_endpoint:start({ws, ua1}, [
+        {from, "\"NkSIP Basic SUITE Test Client\" <sip:client1@nksip>"},
+        {supported, []},
+        {transport, {udp, {0,0,0,0}, 5070}},
+        {transport, {tls, {0,0,0,0}, 5071}}
+    ]),
+
+    ok = sipapp_endpoint:start({ws, ua2}, [
+        {supported, []},
+        {from, "\"NkSIP Basic SUITE Test Client\" <sip:client2@nksip>"},
+        {transport, {ws, {0,0,0,0}, 0}},
+        {transport, {wss, {0,0,0,0}, 8091}}
+    ]),
+
+    tests_util:log(),
+    ?debugFmt("Starting ~p", [?MODULE]).
+
+
+stop() ->
+    ok = nksip:stop_all(),
+    error = sipapp_server:stop({ws, server1}),
+    error = sipapp_endpoint:stop({ws, ua1}),
+    error = sipapp_endpoint:stop({ws, ua2}),
+    ok.
+
+
+basic() ->
+    nksip_uac:options({ws, ua2}, "<sip:127.0.0.1:8080;transport=ws>", []).
+
 
 
 
