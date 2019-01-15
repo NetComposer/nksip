2ce342eb76f2a0e55b1c9fb4e6f3b8e118a1b875
Carlos Gonzalez <carlosj.gf@gmail.com>
2019-01-15 11:24:57 +0100

    Small cleanup

--- src/nksip_sup.erl
+++ src/nksip_sup.erl
@@ -24,70 +24,18 @@
 
 -behaviour(supervisor).
 
-% -export([start_sipapp/2, stop_sipapp/1]).
 -export([init/1, start_link/0]).
 
 -include("nksip.hrl").
 
-% %% @private Starts a new Service's process
-% start_sipapp(SrvId, Args) ->
-%     Spec = {SrvId,
-%                 {nksip_sipapp_sup, start_link, [SrvId, Args]},
-%                 permanent,
-%                 infinity,
-%                 supervisor,
-%                 [nksip_sipapp_sup]},
-%     case supervisor:start_child(nksip_sipapp_sup, Spec) of
-%         {ok, _SupPid} -> ok;
-%         {error, {Error, _}} -> {error, Error};
-%         {error, Error} -> {error, Error}
-%     end.
-
-
-% %% @private Stops a Service's core
-% stop_sipapp(SrvId) ->
-%     case supervisor:terminate_child(nksip_sipapp_sup, SrvId) of
-%         ok -> ok = supervisor:delete_child(nksip_sipapp_sup, SrvId);
-%         {error, _} -> error
-%     end.
-
-
 %% @private
 start_link() ->
     ChildsSpec = [
-        % {nksip_dns,
-        %     {nksip_dns, start_link, []},
-        %     permanent,
-        %     5000,
-        %     worker,
-        %     [nksip_dns]},
-        % {nksip_webserver_sup,
-        %     {nksip_webserver_sup, start_link, []},
-        %     permanent,
-        %     infinity,
-        %     supervisor,
-        %     [nksip_webserver_sup]},
-        % {nksip_webserver,
-        %     {nksip_webserver, start_link, []},
-        %     permanent,
-        %     5000,
-        %     worker,
-        %     [nksip_webserver]}
-        % {nksip_sipapp_sup,
-        %     {?MODULE, start_sipapps_sup, []},
-        %     permanent,
-        %     infinity,
-        %     supervisor,
-        %     [?MODULE]}
-     ] 
+     ]
      ++
      [get_call_routers(N) || N <- lists:seq(0, nksip_config:get_config(msg_routers)-1)],
   supervisor:start_link({local, ?MODULE}, ?MODULE, {{one_for_one, 10, 60}, ChildsSpec}).
 
-% %% @private
-% start_sipapps_sup() ->
-%     supervisor:start_link({local, nksip_sipapp_sup}, 
-%                             ?MODULE, {{one_for_one, 10, 60}, []}).
 
 %% @private
 init(ChildSpecs) ->
